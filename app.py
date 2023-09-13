17add143f926b92390df2a0a090a2607fcad9cb4
Yang <1178574011@qq.com>
2023-09-13 21:54:54 +0800

    update schedule func

--- app.py
+++ app.py
@@ -9,27 +9,28 @@ load_dotenv()
 
 
 def get_miner_data(url, miner_json, miner_csv, keep_keys):
+    logger.info("start sending request!")
     send_request_to_url(url, miner_json)
     extract_json_to_csv(miner_json, miner_csv, keep_keys)
     write_to_database_by_csv(miner_csv)
+    logger.info("finish sending request!")
 
 
-if __name__ == '__main__':
-    url = os.getenv("MINER_URL")
-    miner_json = os.getenv("MINER_JSON_FILE")
-    miner_csv = os.getenv("MINER_CSV_FILE")
-    # 定义需要保留的键名列表
-    keep_keys = ["address", "reachability", "verifiedPrice", "minPieceSize", "maxPieceSize", "rawPower", "isoCode",
-                 "region", "score", "freeSpace", "averagePrice", "rank"]
-    logger.info("start sending request!")
+url = os.getenv("MINER_URL")
+miner_json = os.getenv("MINER_JSON_FILE")
+miner_csv = os.getenv("MINER_CSV_FILE")
 
-    # get_miner_data(url, miner_json, miner_csv, keep_keys)
+# 定义需要保留的键名列表
+keep_keys = ["address", "reachability", "verifiedPrice", "minPieceSize", "maxPieceSize", "rawPower", "isoCode",
+             "region", "score", "freeSpace", "averagePrice", "rank"]
+logger.info("start sending request!")
 
-    # # 定义每天发送请求的任务
-    schedule.every().day.at("19:00").do(get_miner_data)
-    # 循环执行任务
-    while True:
-        logger.info("start sending request!")
-        schedule.run_pending()
-        time.sleep(1)
-        logger.info("finish sending request!")
+
+# 定义每天发送请求的任务
+schedule.every().day.at("19:00").do(get_miner_data, url, miner_json, miner_csv, keep_keys)
+
+
+# 循环执行任务
+while True:
+    schedule.run_pending()
+    time.sleep(1)
