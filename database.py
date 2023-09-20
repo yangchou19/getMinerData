0df38a6171a46c94ff68bb2b730c7088696d2839
lsy2021-07 <1071lsy@shu.edu.cn>
2023-09-20 16:30:55 +0800

    11

--- database.py
+++ database.py
@@ -104,3 +104,51 @@ def write_to_database_by_csv(csv_file):
     # 提交事务并关闭连接
     conn.commit()
     conn.close()
+
+
+def write_to_database_by_csv_pingminers(csv_file):
+    print(csv_file)
+    # 连接到 SQLite 数据库
+    # jdbc:mysql://124.70.130.44:3306/blockchain?useSSL=false
+    conn = mysql.connector.connect(
+        host=os.getenv('HOST'),
+        port=os.getenv('PORT'),
+        user=os.getenv('USER'),
+        password=os.getenv('PASSWORD'),
+        database=os.getenv('DATABASE')
+    )
+
+    table_name = 'pingminers'
+    database = os.getenv('DATABASE')
+    # 创建一个游标对象
+    cursor = conn.cursor()
+
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    logger.info(f"Table '{table_name}' exists and has been deleted.")
+
+    # 创建数据表（如果不存在）
+    cursor.execute(f'''CREATE TABLE IF NOT EXISTS {table_name} (
+                        address TEXT,
+                        minPieceSize, TEXT,
+                        maxPieceSize TEXT,
+                        Price TEXT,
+                        VerifiedPrice TEXT,
+                        Ping TEXT, 
+                    )''')
+
+    # 插入数据
+    sql = f'''INSERT INTO {table_name} (address, minPieceSize, maxPieceSize, Price, VerifiedPrice, Ping)
+             VALUES (%s, %s, %s, %s, %s, %s)'''
+
+    # 读取 CSV 文件并插入数据
+    with open(csv_file, 'r') as file:
+        csv_data = csv.reader(file)
+        count = 0
+        for row in csv_data:
+            cursor.execute(sql, row)
+            count +=1
+        print(count)
+    logger.info(f"Table '{table_name}' has been writen successful.")
+    # 提交事务并关闭连接
+    conn.commit()
+    conn.close()
